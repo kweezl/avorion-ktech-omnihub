03d1900d2c4cdd5da314d71d39a99596135150e0
kweezl <igorpieco@gmail.com>
2026-06-06 01:01:45 +0400

    feat: extend station founder to include OmniHub entry

---
+++ data/scripts/entity/stationfounder.lua
@@ -0,0 +1,13 @@
+-- OmniHub mod: appends OmniHub to the Found Station > Other Stations list.
+-- This file is concatenated by the engine after the vanilla stationfounder.lua.
+-- StationFounder and StationFounder.stations are already defined above.
+
+table.insert(StationFounder.stations, {
+    name    = "OmniHub"%_t,
+    tooltip = "A modular production station. Install factory modules to produce goods. Modules can be bought from an OmniHub Supplier."%_t,
+    scripts = {
+        { script = "data/scripts/entity/merchants/omnihubcontroller.lua" },
+        { script = "data/scripts/entity/merchants/omnihubsupplier.lua" },
+    },
+    price = 15000000,
+})
